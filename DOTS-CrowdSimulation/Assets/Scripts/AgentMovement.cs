0bb82a3e0d2ce1a89e47060c695b144eb1ec68a7
Diego "Dídac" Campos Milian <didaccampos@gmail.com>
2020-01-30 00:42:30 +0100

    Testing Things

---
+++ DOTS-CrowdSimulation/Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+[AlwaysSynchronizeSystem]
+public class AgentMovement : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities.ForEach((ref Translation trans , in AgentData inData) =>
+        {
+            Vector3 velocity = inData.direction * inData.speed * deltaTime;
+            trans.Value.x = trans.Value.x + velocity.x;
+            trans.Value.y = trans.Value.y + velocity.y;
+            trans.Value.z = trans.Value.z + velocity.z;
+        }).Run();
+
+        return default;
+    }
+
+}
---
+++ DOTS-CrowdSimulation/Assets/Scripts/DataComponets/AgentData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct AgentData : IComponentData
+{
+    public Vector3 destination;
+    public Vector3 direction;
+    public float speed;
+}
